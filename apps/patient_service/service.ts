3f0ca98a55a2c71c61bff1abe72580e954e4d281
UriCW <Uri.Cohen.Waisel@Gmail.com>
2024-05-26 17:50:23 +0200

    base service code

---
+++ apps/patient_service/service.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import apicache from "apicache";
+import { getAllRecords, updateRecords, seedDatabase } from './utils'
+import 'dotenv/config'
+
+const app = express();
+let cache = apicache.middleware;
+app.use(cache("1 minutes"));
+
+const port = process.env.SERVICE_PORT || 3000;
+const endpoint = process.env.ENDPOINT_URL || ""
+
+if (!endpoint){
+  throw new Error("Environment variable ENDPOINT_URL not defined, quitting");
+}
+
+app.get("/update", async (_, response) => {
+  updateRecords(endpoint).then(() => {
+    response.send({ status: "SUCCESS", message: "Updated patient reading records on database" });
+  });
+
+});
+
+app.get("/all", async (_, response) => {
+  const allRecords = await getAllRecords()
+  response.send(allRecords);
+});
+
+app.listen(port, () => {
+  seedDatabase(endpoint);
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ apps/patient_service/utils.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function updateRecords(endpoint: string) {
+  console.log("update() called");
+  const response = await axios.get(endpoint);
+  for (var reading of response.data) {
+    reading.date_testing = new Date(reading.date_testing);
+    reading.date_birthdate = new Date(reading.date_birthdate);
+    await prisma.patientRecord.create({ data: reading });
+  }
+}
+
+export async function getAllRecords() {
+  const allRecords = await prisma.patientRecord.findMany({
+    orderBy: {
+      date_testing: "asc",
+    },
+  });
+  return allRecords;
+}
+
+export async function numberOfClientsInDatabase() {
+  const n = await prisma.patientRecord.groupBy({
+    by: "client_id"
+  })
+  return n.length
+}
+
+
+export async function seedDatabase(endpoint: string) {
+  var numClients = await numberOfClientsInDatabase()
+  while (numClients <= 10) {
+    console.log(`Database contains ${numClients} seeding again`);
+    numClients = await numberOfClientsInDatabase()
+    await updateRecords(endpoint);
+  }
+}
