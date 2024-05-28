0e3e56aa2b1f71c137be239a870e27b45a02a684
UriCW <Uri.Cohen.Waisel@Gmail.com>
2024-05-28 16:05:53 +0200

    Handling missing endpoint at buildtime better

--- apps/patient_frontend/app/all_records/route.ts
+++ apps/patient_frontend/app/all_records/route.ts
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server";
-import 'dotenv/config'
+import "dotenv/config";
 
-const endpoint = process.env.SERVICE_ENDPOINT
-export async function GET(){
-  const response = await fetch(endpoint);
-  const ret = await response.json()
-  return NextResponse.json(ret);
+const endpoint = process.env.SERVICE_ENDPOINT;
+export async function GET() {
+  try {
+    const response = await fetch(endpoint);
+    const ret = await response.json();
+    return NextResponse.json(ret);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({message: "Error getting records from service"});
+  }
 }
