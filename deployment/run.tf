7d6a5234b217a5b267a015b53723e144a4c009ed
UriCW <Uri.Cohen.Waisel@Gmail.com>
2024-05-28 22:13:59 +0200

    Create tables maybe?

--- deployment/run.tf
+++ deployment/run.tf
@@ -1,30 +1,34 @@
-# resource "google_cloud_run_v2_service" "biocomposition_service" {
-#   name     = "biocomposition-service"
-#   location = var.region
-#   ingress = "INGRESS_TRAFFIC_ALL"
-# 
-#   template {
-#     containers {
-#       image = "europe-southwest1-docker.pkg.dev/innohealthexcercise/innohealth/bioservice"
-#       ports {
-#         container_port = 3000
-#       }
-#       env {
-#         name = "ENDPOINT_URL"
-#         value = "https://mockapi-furw4tenlq-ez.a.run.app/data"
-#       }
-#     }
-#   }
-# }
-# 
-# resource "google_cloud_run_v2_service" "biocomposition_frontend" {
-#   name     = "biocomposition-frontend"
-#   location = var.region
-#   ingress = "INGRESS_TRAFFIC_ALL"
-# 
-#   template {
-#     containers {
-#       image = "europe-southwest1-docker.pkg.dev/innohealthexcercise/innohealth/biofrontend"
-#     }
-#   }
-# }
+resource "google_cloud_run_v2_service" "biocomposition_service" {
+  name     = "biocomposition-service"
+  location = var.region
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "europe-southwest1-docker.pkg.dev/innohealthexcercise/innohealth/bioservice"
+      ports {
+        container_port = 3000
+      }
+      env {
+        name = "ENDPOINT_URL"
+        value = "https://mockapi-furw4tenlq-ez.a.run.app/data"
+      }
+      env {
+        name = "DATABASE_URL"
+        value = "postgresql://user:password@localhost:5432/innohealthdb?host=/cloudsql/innohealthexcercise:europe-southwest1:patients"
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_v2_service" "biocomposition_frontend" {
+  name     = "biocomposition-frontend"
+  location = var.region
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "europe-southwest1-docker.pkg.dev/innohealthexcercise/innohealth/biofrontend"
+    }
+  }
+}
