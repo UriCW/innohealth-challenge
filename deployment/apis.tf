0d42102a9808cbac2c6556f522722a4db13adc7a
UriCW <Uri.Cohen.Waisel@Gmail.com>
2024-05-28 21:11:24 +0200

    Added some networking

--- deployment/apis.tf
+++ deployment/apis.tf
@@ -4,9 +4,9 @@
 # resource "google_project_service" "cloudresourcemanager" {
 #   service = "cloudresourcemanager.googleapis.com"
 # }
-# resource "google_project_service" "computeapi" {
-#   service = "compute.googleapis.com"
-# }
+resource "google_project_service" "computeapi" {
+  service = "compute.googleapis.com"
+}
 # resource "google_project_service" "alloydbapi" {
 #   service = "alloydb.googleapis.com"
 # }
--- deployment/run.tf
+++ deployment/run.tf
@@ -1,26 +1,30 @@
-resource "google_cloud_run_v2_service" "biocomposition_service" {
-  name     = "biocomposition-service"
-  location = var.region
-  ingress = "INGRESS_TRAFFIC_ALL"
-
-  template {
-    containers {
-      image = "europe-southwest1-docker.pkg.dev/innohealthexcercise/innohealth/bioservice"
-      ports {
-        container_port = 3000
-      }
-    }
-  }
-}
-
-resource "google_cloud_run_v2_service" "biocomposition_frontend" {
-  name     = "biocomposition-frontend"
-  location = var.region
-  ingress = "INGRESS_TRAFFIC_ALL"
-
-  template {
-    containers {
-      image = "europe-southwest1-docker.pkg.dev/innohealthexcercise/innohealth/biofrontend"
-    }
-  }
-}
+# resource "google_cloud_run_v2_service" "biocomposition_service" {
+#   name     = "biocomposition-service"
+#   location = var.region
+#   ingress = "INGRESS_TRAFFIC_ALL"
+# 
+#   template {
+#     containers {
+#       image = "europe-southwest1-docker.pkg.dev/innohealthexcercise/innohealth/bioservice"
+#       ports {
+#         container_port = 3000
+#       }
+#       env {
+#         name = "ENDPOINT_URL"
+#         value = "https://mockapi-furw4tenlq-ez.a.run.app/data"
+#       }
+#     }
+#   }
+# }
+# 
+# resource "google_cloud_run_v2_service" "biocomposition_frontend" {
+#   name     = "biocomposition-frontend"
+#   location = var.region
+#   ingress = "INGRESS_TRAFFIC_ALL"
+# 
+#   template {
+#     containers {
+#       image = "europe-southwest1-docker.pkg.dev/innohealthexcercise/innohealth/biofrontend"
+#     }
+#   }
+# }
